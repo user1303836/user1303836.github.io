5774fee4695a984b50b1604ace8f4b65748ef9f7
user1303836 <gcvvvvvv@gmail.com>
2024-07-01 18:35:25 -0400

    hi3

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,8 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>bruh</h1>
       </div>
-      <h1>bruh</h1>
     </>
   );
 }
