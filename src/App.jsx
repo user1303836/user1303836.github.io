0dff13ad81dfe13780033e6380d223f6a28338d4
user1303836 <gcvvvvvv@gmail.com>
2024-07-02 11:59:50 -0400

    hi5

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,30 @@
 import React from "react";
-import "./App.css";
 
 function App() {
   return (
-    <div className="app-container">
-      <div className="sidebar">
-        <h1>testtext</h1>
+    <div className="flex min-h-screen font-system">
+      <div className="w-64 p-6 bg-gray-100">
+        <h2
+          href="/"
+          className="text-1xl font-bold mb-10 cursor-pointer hover:text-gray-600"
+        >
+          jw
+        </h2>
         <nav>
-          <ul>
-            <li>projects</li>
-            <li>about</li>
-            <li>misc</li>
+          <ul className="space-y-4">
+            <li className="cursor-pointer hover:text-gray-600 transition-colors">
+              projects
+            </li>
+            <li className="cursor-pointer hover:text-gray-600 transition-colors">
+              about
+            </li>
+            <li className="cursor-pointer hover:text-gray-600 transition-colors">
+              misc
+            </li>
           </ul>
         </nav>
       </div>
-      <div className="main-content">{/* Main content will go here */}</div>
+      <div className="flex-grow p-6">{/* Main content will go here */}</div>
     </div>
   );
 }
