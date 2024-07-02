26cbd471969d451c0e1eb2c4122dfd9919b17dab
user1303836 <gcvvvvvv@gmail.com>
2024-07-02 16:50:46 -0400

    hi67

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,31 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
+import anime from "animejs";
 
 function App() {
+  const navRef = useRef(null);
+
+  useEffect(() => {
+    anime({
+      targets: navRef.current.children,
+      translateY: [0, 1],
+      opacity: [0, 1],
+      delay: anime.stagger(100),
+      easing: "easeOutElastic",
+      duration: 2500,
+    });
+  }, []);
+
   return (
     <div className="flex min-h-screen font-system">
-      <div className="w-64 p-6 bg-gray-100">
+      <div className="w-40 p-6 bg-gray-50 gradient-overlay">
         <h2
           href="/"
           className="text-1xl font-bold mb-10 cursor-pointer hover:text-gray-600"
         >
           jw
         </h2>
-        <nav>
-          <ul className="space-y-4">
+        <nav className="relative ">
+          <ul ref={navRef} className="space-y-4 relative z-10">
             <li className="cursor-pointer hover:text-gray-600 transition-colors">
               projects
             </li>
